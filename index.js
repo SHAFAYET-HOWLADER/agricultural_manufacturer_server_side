89b743f15d57ea280a3580c8af33a086c19e43de
unknown <you@example.com>
2022-05-24 12:12:53 +0600

    user order loaded from db

--- index.js
+++ index.js
@@ -15,14 +15,61 @@ app.get('/', (req, res) => {
 })
 const uri = `mongodb+srv://${process.env.toolsUser}:${process.env.toolsPass}@cluster0.ipzen.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+//---------------verify jwt----------------//
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unAuthorized access' })
+    }
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden' })
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
 async function run() {
     try {
         await client.connect();
         const toolsCollection = client.db("toolsProducer").collection("tools");
+        const userCollection = client.db("toolsProducer").collection("users");
+        const ordersCollection = client.db("toolsProducer").collection("orders");
+        //get all orders 
+        // app.get('/orders', async (req, res) => {
+        //     const query = {};
+        //     const cursor = ordersCollection.find(query);
+        //     const result = await cursor.toArray();
+        //     res.send(result);
+        // })
+        //get orders of user from db
+        app.get('/orders', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email }
+            const orders = await ordersCollection.find(query).toArray();
+            res.send(orders);
+        })
+
+        //post user's orders information
+        app.post('/orders', async (req, res) => {
+            const customer = req.body;
+            const query = {
+                productName: customer.product,
+                userName: customer.name,
+                userPhone: customer.phone
+            }
+            const exists = await ordersCollection.findOne(query);
+            if (exists) {
+                return res.send({ success: false, orders: exists });
+            }
+            const result = await ordersCollection.insertOne(customer);
+            return res.send({ success: true, result });
+        })
         //find one
-        app.get('/tools/:id', async (req,res)=>{
+        app.get('/tools/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const tool = await toolsCollection.findOne(query);
             res.send(tool);
         })
@@ -32,14 +79,28 @@ async function run() {
             const cursor = toolsCollection.find(query);
             const result = await cursor.toArray();
             res.send(result);
-          })
+        })
+        //create user using put method
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            }
+            const result = await userCollection.updateOne(filter, updateDoc, options)
+            const accessToken = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d',
+            })
+            res.send({ result, accessToken });
+        })
     }
     finally {
         // await client.close();
     }
 }
 run().catch(console.dir);
-
 app.listen(port, () => {
     console.log('Backend server is running', port);
 })
